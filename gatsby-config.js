6c8ffe2749581ccc61873a543e79110ddffc0b57
Lucy <lucy@Lucys-MacBook-Air.local>
2022-11-26 21:19:12 -0600

    removing the repo name from the url

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: 'Manideep Cherukuri',
     description: 'Manideep is an engineer who loves building intelligent systems.',
-    siteUrl: 'https://manideep2705.github.io', // No trailing slash allowed!
+    siteUrl: 'https://manideep1999.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@manideep2705',
   },
